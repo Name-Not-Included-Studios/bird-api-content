1d3fa629b5418ffaa62dd7006ec54c0616dcbcee
ThatColdToast <thatcoldtoast@gmail.com>
2022-12-04 00:48:57 -0600

    i sleep and cz no worky :(

--- src/main/kotlin/app/birdsocial/birdapi/user/UserController.kt
+++ src/main/kotlin/app/birdsocial/birdapi/user/UserController.kt
@@ -1,12 +1,26 @@
 package app.birdsocial.birdapi
 
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+//import org.springframework.web.context.request.RequestAttributes
+//import org.springframework.web.context.request.RequestContextHolder
+
+//import org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST
+
+//TODO Fix everything
 @Controller
 class UserController {
 
-    private UserDao userDao;
+//    private UserDao userDao;
+
+    // @QueryMapping
+    // public recentPosts(@Argument count: Int, @Argument offset: Int): List<Post> {
+    //     return userDao.getRecentPosts(count, offset);
 
     @QueryMapping
-    public recentPosts(@Argument count: Int, @Argument offset: Int): List<Post> {
-        return userDao.getRecentPosts(count, offset);
-    }
+	fun greeting(): String {
+		return "Hello"
+//		val attributes: RequestAttributes? = RequestContextHolder.getRequestAttributes();
+//		return "Hello " + attributes.getAttribute(RequestAttributeFilter.NAME_ATTRIBUTE, SCOPE_REQUEST);
+	}
 }
